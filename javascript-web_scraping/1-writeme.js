73ea098141ebd07ef27575244260c0800abe783e
Silvycr <silvia_alinacr@live.com>
2023-10-22 22:22:19 -0700

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,12 +3,11 @@
   Writes a string to a file.
 */
 const fs = require('fs');
+const firstArg = process.argv[2];
+const content = process.argv[3];
 
-// Contain name of the file
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+fs.writeFile(firstArg, content, 'utf-8', (err) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(process.argv[3]);
+    console.error(err);
   }
 });
