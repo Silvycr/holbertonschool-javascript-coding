a65396bed209f23e1cbdbde6bb733d1599fe5560
Silvycr <silvia_alinacr@live.com>
2023-10-22 22:34:42 -0700

    How many completed?

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,49 +1,21 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 
-const movieId = process.argv[2];
+const url = process.argv[2];
 
-if (!movieId) {
-  console.log('Please provide a valid Movie ID as the first argument.');
-  process.exit(1);
-}
+request(url, (error, res, body) => {
+  if (error) console.error(error);
 
-const apiUrl = `https://swapi-api.hbtn.io/films/${movieId}/`;
+  const data = JSON.parse(body);
+  const results = {};
 
-axios
-  .get(apiUrl)
-  .then((response) => {
-    const movie = response.data;
-
-    const expectedCharacterOrder = [
-      'Darth Vader',
-      'R2-D2',
-      'Luke Skywalker',
-      'Han Solo',
-      'Leia Organa',
-      'Chewbacca',
-      'Palpatine',
-      'Obi-Wan Kenobi',
-      'Jabba Desilijic Tiure',
-      'Wedge Antilles',
-      'Yoda',
-      'Boba Fett',
-      'Ackbar',
-      'Arvel Crynyd',
-      'Mon Mothma',
-      'Nien Nunb',
-      'Wicket Systri Warrick',
-      'Bib Fortuna',
-      'C-3PO',
-      'Lando Calrissian',
-    ];
-
-    expectedCharacterOrder.forEach((characterName) => {
-      console.log(characterName);
-    });
-  })
-  .catch((error) => {
-    console.error('Error:', error.message);
-    process.exit(1);
+  data.forEach(task => {
+    if (task.completed) {
+      if (results[task.userId]) results[task.userId]++;
+      else results[task.userId] = 1;
+    }
   });
+
+  console.log(results);
+});
